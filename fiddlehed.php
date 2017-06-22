fb45b2b23303a7c680713fba0bee748e79fc394c
tarabusk <tarabusk@gmail.com>
2017-06-22 07:36:13 -0700

    Correction

--- fiddlehed.php
+++ fiddlehed.php
@@ -412,7 +412,7 @@ function nav_menu_add_post_status_class($classes, $item){
 
 
   $terms = get_the_terms( get_the_ID(), 'fiddlehed-restriction' );
-
+  $class_restriction = '';
   if ( $terms && ! is_wp_error( $terms ) ) :
 
       $restriction_slug = array();
